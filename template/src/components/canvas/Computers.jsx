27b4d539b2043d335bf95b75d5f15fedce7629aa
Jacob Louis <louisjacob8@gmail.com>
2023-09-21 10:25:20 +0530

    fix: change the computer model to responsive

--- template/src/components/canvas/Computers.jsx
+++ template/src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import {OrbitControls,Preload,SpotLight,useGLTF} from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({ismobile}) => {
 
   const computer = useGLTF('./desktop_pc/scene.gltf')
 
@@ -21,14 +21,36 @@ const Computers = () => {
       />
       <pointLight intensity={1} />
       <primitive object={computer.scene} 
-        scale={1} 
-        position={[0,-3.25,-1.5]}
+        scale={ismobile? 0.7 :1} 
+        position={ismobile? [0,-3,-2.2] :[0,-3.25,-1.5]}
         rotation={[-0.01,-0.2,-0.1]}/>
     </mesh>
   )
 }
 
 const ComputerCanvas = ()=>{
+
+  const [ismobile,setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+
+    setIsMobile(mediaQuery.matches)
+
+    const handleMediaQueryChange = (event)=>{
+      setIsMobile(event.matches)
+    }
+
+    mediaQuery.addEventListener('change',handleMediaQueryChange)
+
+    return ()=>{
+      mediaQuery.removeEventListener('change',handleMediaQueryChange)
+    }
+
+  }, [])
+  
+
+
   return(
     <Canvas frameloop='demand' shadows
       camera={{position:[20,3,5], fov:25}} 
@@ -40,7 +62,7 @@ const ComputerCanvas = ()=>{
           maxPolarAngle={Math.PI/2} 
           minPolarAngle={Math.PI/2} 
         />
-        <Computers/>
+        <Computers ismobile={ismobile} />
       </Suspense>
       <Preload all/>
     </Canvas>
